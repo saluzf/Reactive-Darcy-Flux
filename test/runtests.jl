98bb3d9da2f8dfd028c198db6903da3c20c47664
saluzf <saluzf@ethz.ch>
2024-01-15 23:30:31 +0100

    update reference test

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,12 @@
 using FinalProject
 using Test
 
+
 @testset "FinalProject.jl" begin
-    # Write your tests here.
+    exename = joinpath(Sys.BINDIR, Base.julia_exename())
+    testdir = pwd()
+    # run test scripts:
+    run(`$exename -O3 --startup-file=no $(joinpath(testdir, "testIsothermalSalineAquifier.jl"))`)
+    run(`$exename -O3 --startup-file=no $(joinpath(testdir, "testSalineAquifier.jl"))`)
+    run(`$exename -O3 --startup-file=no $(joinpath(testdir, "testReactiveSalineAquifier.jl"))`)
 end
